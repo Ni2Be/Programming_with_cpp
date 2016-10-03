3d0624aca9447b74dd047cc4028ee44e711aa588
Nice2Bee <nice2beeg@gmail.com>
2016-10-03 13:00:18 +0200

    FLTK

--- Chapter 12 simple gui/newSkills/newSkills/main.cpp
+++ Chapter 12 simple gui/newSkills/newSkills/main.cpp
@@ -1,11 +1,24 @@
-#include "GUI.h"
-#include "Layout.h"
+//#include "GUI.h"
+//#include "Layout.h"
 
-void callback1(Fl_Widget*, void*){}
+//void callback1(Fl_Widget*, void*){}
+//#include "Point.h"
+#include "Window.h"
+//#include "Graph.h"
+//#include "fltk.h"
+#include <string>
 
+//using namespace Graph_lib;
 
 int main() {
+	//Point p(100, 100);
 
+	Graph_lib::Window window1(400, 400, "Hallo");
+	
+//	Line l1(Point(100, 100), Point(200, 200));
+
+//	window1.attach(l1);
+	/*
 	Windows window(Point(100, 100), 400, 400, "Best GUI!");
 	Line xaxis(Point(50, 350), Point(350, 350), 1, FL_BLACK);
 	Line yaxis(Point(50, 350), Point(50, 50), 1, FL_BLACK);
@@ -25,7 +38,5 @@ int main() {
 	
 	window.attach(slider1);
 	window.attach(button1);
-
-
-	return Fl::run();
+*/
 }
