3a45029086d4feee38b138d71bf0585adf6b41dc
nice2bee <nice2beeg@gmail.com>
2016-10-07 10:40:55 +0200

    stuck with lines

--- Chapter 12 simple gui/FLTK lib/Test/Test/main.cpp
+++ Chapter 12 simple gui/FLTK lib/Test/Test/main.cpp
@@ -1,11 +1,14 @@
 #include "IO_Window.h"
-
+#include "Shape.h"
 
 using namespace TestGUI;
 
 int main() 
 {
-	IO_Window window1(300, 200, "Test");
-	//IO_Window window2(300, 200, "Test");
+	IO_Window *window1[1];
+	for (int i = 0; i < 1; i++) {
+		window1[i] = new IO_Window(350, 180, "Test");
+		Draw::Line(10, 10, 100, 100);
+	}
 	return Fl::run();
 }
