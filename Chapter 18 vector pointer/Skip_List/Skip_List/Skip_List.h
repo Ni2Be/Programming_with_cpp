5cc1b8595a6a2262d72efcda9510273dddafa801
Nice2BeeDesktop <nice2beeg@gmail.com>
2017-03-09 16:23:24 +0100

    finished Skip Listl

--- Chapter 18 vector pointer/Skip_List/Skip_List/Skip_List.h
+++ Chapter 18 vector pointer/Skip_List/Skip_List/Skip_List.h
@@ -2,99 +2,135 @@
 Skip_List.h
 Purpose: 
 Skip List with the functions to
-add: add(int key), delete: del(int key) 
-and search: search(int key).
+add: add(int key), get front elem: front() 
+and pop front elem: pop_front().
+
+Attention:
+the vector that contains the Nodes
+goes from 0 to MAXLEVEL - 1
+where MAXLEVEL - 1 is the bottem of the list
+and 0 is the top:
+
+0				| o - - - - - -
+1				| o - - o - - -
+[...]			| o - - o - - -
+MAXLEVEL - 2	| o - o o - o -
+MAXLEVEL - 1	| o o o o o o o
 
 @author Jordan
-@version 0.1 03/02/17
+@version 1.0 03/02/17
 */
 
 #pragma once
 #include <vector>
 #include <iostream>
+#include <math.h>
 
-class Skip_Node
+#include <random>
+
+#ifdef _DEBUG
+#define DEBUG
+#endif // _DEBUG
+
+namespace N2B
 {
-	friend class Skip_List;
-private:
-	Skip_Node()
-		:key(), next() {}
-	Skip_Node(int k)
-		:key(k), next() {}
-	Skip_Node(Skip_Node& node)
-		:key(node.key), next(node.next) {}
 
-	std::vector<Skip_Node*> next;
-	int key;
-};
-
-class Skip_List
-{
-public:
-	Skip_List()
-	:root(0) {}
-	~Skip_List()
-	{
-		delete_tree();
-	}
-
-	/**
-	Adds a node to the Skip List.
-
-	@param Key of the new Node
-	@return none
-	*/
-	void add(int key);
-	
-	/**
-	Searches a node by his key and deletes it.
-
-	@param searched key
-	@return true if a node was deleted, 
-	false if no node was found. 
-	*/
-	bool del(int key);
-	
-	/**
-	Searches a node by his key.
-
-	@param searched key
-	@return -1 if no Node was found, else key of the searched node
-	*/
-	int search(int key);
-private:
 	const int MAXLEVEL = 25;
-	Skip_Node* root;
 
-	/**
-	Goes through the list till it findes the searched key
-	and returns the Node reference or the node to its left 
-	if there is no node with the searchd key.
+	class Skip_Node
+	{
+		friend class Skip_List;
+	private:
+		Skip_Node(int k)
+			:key(k), next(MAXLEVEL) {}
 
-	@param searched key
-	@return Node reference
-	*/
-	Skip_Node& intern_search(int key, std::vector<Skip_Node*>&);
-	
-	/**
-	Goes through every node of the list 
-	and calls the destructor for each.
+		std::vector<Skip_Node*> next;
+		int key;
+	};
 
-	@param none
-	@return none
-	*/
-	void delete_tree();
-	
-	/**
-	Generates a number that stands for the level
-	of a new node. 
-	The chance to go 
-	to level 2 is 0.25,
-	to level 3 is 0.25²
-	and so on.
+	class Skip_List
+	{
+	public:
+		Skip_List() {}
+		~Skip_List()
+		{
+			delete_tree();
+		}
 
-	@param none
-	@return integer 1 to MAXLEVEL
-	*/
-	int determine_level(); 
-};
+		/**
+		Adds a node to the Skip List.
+
+		@param Key of the new Node
+		@return none
+		*/
+		void add(int key);
+
+		/**
+		Returns the key of the root Node.
+
+		@param none
+		@return -1 if list is empty, else key of the front node
+		*/
+		int front();
+
+		/**
+		Returns true if the list is empty.
+
+		@param none
+		@return true if the list is empty
+		*/
+		bool is_empty();
+
+		/**
+		Pops the root of the list, does nothing if list is empty.
+
+		@param none
+		@return none
+		*/
+		void pop_front();
+
+	private:
+		Skip_Node* root;
+
+		/**
+		Goes through the list till it findes the new position of
+		the searched key. It will store all Nodes that will maybe
+		need to point to the new node in the returned vector.
+
+		@param searched key
+		@return Node reference
+		*/
+		std::vector<Skip_Node*> intern_search(int key);
+
+		/**
+		Inserts a new node between the nodes in level_nodes
+		and their successors
+
+		@param int key of the new node, vector<Skip_Node*>& level_nodes
+		@return none
+		*/
+		void insert_node(int key, std::vector<Skip_Node*>& level_nodes);
+
+		/**
+		Goes through every node of the list
+		and calls the destructor for each.
+
+		@param none
+		@return none
+		*/
+		void delete_tree();
+
+		/**
+		Generates a number that stands for the level
+		of a new node.
+		The chance to go
+		to level 2 is 0.25,
+		to level 3 is 0.25²
+		and so on.
+
+		@param none
+		@return integer 0 to MAXLEVEL - 1
+		*/
+		int determine_level();
+	};
+}
--- Chapter 18 vector pointer/Skip_List/Skip_List/main.cpp
+++ Chapter 18 vector pointer/Skip_List/Skip_List/main.cpp
@@ -5,7 +5,16 @@ int main()
 {
 	try
 	{
-		Skip_List list1();
+		N2B::Skip_List list1;
+		for (int i = 0; i < 10; i++)
+		{
+			list1.add(i);
+		}
+		while (!list1.is_empty())
+		{
+			std::cout << list1.front();
+			list1.pop_front();
+		}
 	}
 	catch (std::exception &e)
 	{
